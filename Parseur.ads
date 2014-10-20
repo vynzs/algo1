81437239972a19cea741b9df67020224a90c1880
vynzs <bmqr9qy@hotmail.com>
2014-10-21 01:34:59 +0200

    Fichiers Ads

---
+++ Parseur.ads
@@ -0,0 +1,6 @@
+package Parseur is
+   
+   procedure Lecture_En_Tete(Nom_Entree :in String; Nombre_Objets, Largeur_Ruban : out Integer);
+   procedure Lecture(Nom_Entree : in String, Objets : in Objet) ; -- SQUA UN OBJET ? 
+   
+end Parseur;
--- Svg.ads
+++ Svg.ads
@@ -2,4 +2,4 @@ package Svg is
    
    procedure Sauvegarde (Sortie : in String; Objets : in Tableau_Objets; Largeur, Hauteur : in Integer) -- Ecrit le Svg de sortie
 
-end MoyennePonderee ;Svg.Sauvegarde(Argument(2), Objets, Largeur_Ruban, Hauteur_Ruban
+end Svg;
